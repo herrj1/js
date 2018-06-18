b75ee4def4379acfbf5f2cb52293ef8d7b44b5bd
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-17 21:47:44 -0400

    Added prototype implementation

---
+++ patterns/prototypePattern.js
@@ -0,0 +1,13 @@
+var MazdaModels = function(){
+	this.numWheels = 4;
+	this.manufacturer = 'Mazda';
+	this.make = 'Coupe';
+}
+
+MazdaModels.prototype.go = function(){
+	console.log("Car is moving forward.");
+}
+
+MazdaModels.prototype.stop = function(){
+	console.log("Car is stopped.");
+}
