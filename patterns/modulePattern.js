0eeff50c9259ae5f6d0f2fa2e8494a5713ae28ca
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-17 21:46:40 -0400

    Added module implementation

---
+++ patterns/modulePattern.js
@@ -0,0 +1,18 @@
+var HTMLChanger = (function(){
+	var contents = 'contents';
+	
+	var changeHTML = function(){
+		var element = document.getElementById('attribute-to-change');
+		element.innerHTML = contents;
+	}
+	
+	return{
+		callChangeHTML: function(){
+		   changeHTML();
+		   console.log(contents);
+		}
+	};
+})();
+
+HTMLChanger.callChangeHTML();
+console.log(HTMLChanger.contents);
