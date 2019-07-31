e6283ab835e776056453ebd0b9750d6f60b3b085
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-07-30 23:28:01 -0400

    Update app.js

--- angular/app1/app.js
+++ angular/app1/app.js
@@ -1,9 +1,4 @@
-*
-
-Simple blog front end demo in order to learn AngularJS - You can add new posts, add comments, and like posts.
-
-*/
-
+*Simple blog front end demo in order to learn AngularJS - You can adds new posts, add comments, and like posts.*/
 (function(){
   var app = angular.module('blogApp',[]);
   
