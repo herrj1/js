75cd1c94c94f345639442afe5b65280ada44f621
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-05-31 23:23:18 -0400

    Update Set.js

--- DataStructures/Set.js
+++ DataStructures/Set.js
@@ -2,12 +2,14 @@ function Set(){
   this.values = [];
   this.numberOfValues = 0;
 }
+
 Set.prototype.add = function (value) {
 	if(!~this.values.indexOf(value)){
 		this.values.push(value);
 		this.numberOfValues++;	
 	}
 };
+
 Set.prototype.remove = function (value) {
 	var index = this.values.indexOf(value);
 	if (~index) {
