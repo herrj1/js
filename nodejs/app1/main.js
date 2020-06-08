bdc8e99e6203ab6b22fe24c6628bc93148299c4b
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-06-07 23:48:38 -0400

    Update main.js

--- nodejs/app1/main.js
+++ nodejs/app1/main.js
@@ -2,10 +2,8 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 const{app, BrowserWindow, Menu, ipcMain} = electron;
-
 let mainWindow;
 let addWindow;
-
 app.on('ready', function(){
 	mainWindow = new BrowserWindow({
 		width: 400,
