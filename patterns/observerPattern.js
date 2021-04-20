6067d2dbd2b6568887b50a4687065e2b602b5990
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-04-19 23:49:31 -0400

    Update observerPattern.js

--- patterns/observerPattern.js
+++ patterns/observerPattern.js
@@ -1,9 +1,11 @@
 $scope.$on('nameChanged', function(event, args){
 	$scope.name = args.name;
 });
+
 $scope.userNameChanged = function(name){
 	$scope.$emit('nameChanged', {name: name});
 };
+
 var Subject = function(){
 	this.observers = [];
 	return{	subscribeObserver: function(observer){
