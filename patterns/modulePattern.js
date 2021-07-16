a9533f79d633b942c393b6493936e8af9724a25f
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-07-15 20:57:26 -0400

    Update modulePattern.js

--- patterns/modulePattern.js
+++ patterns/modulePattern.js
@@ -1,5 +1,6 @@
 var HTMLChanger = (function(){
 	var contents = 'contents';
+	
 	var changeHTML = function(){
 		var element = document.getElementById('attribute-to-change');
 		element.innerHTML = contents;
