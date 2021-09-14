c1c816496992314f8b07ed90ec8addd231c845d8
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-09-13 23:15:22 -0400

    Update prototypePattern.js

--- patterns/prototypePattern.js
+++ patterns/prototypePattern.js
@@ -1,8 +1,11 @@
 var MazdaModels = function(){
 	this.numWheels = 4;
+	
 	this.manufacturer = 'Mazda';
+	
 	this.make = 'Coupe';
 }
+
 MazdaModels.prototype.go = function(){
 	console.log("Car is moving forward.");
 }
